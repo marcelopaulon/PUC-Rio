a87446698bbb4f2e7faede6dc8f43c2122316c8f
Marcelo <Marcelo>
2016-05-09 17:57:58 -0300

    Estrutura e renderização inicial do mapa

---
+++ 1411029-1412122/src/rendering/ConstantsEnum.java
@@ -0,0 +1,11 @@
+package rendering;
+
+public class ConstantsEnum {
+
+	public static final int rectSizeX = 255;
+	public static final int rectSizeY = 255;
+	
+	public static final float yardSizeX = (rectSizeX / 15) * 12;
+	public static final float yardSizeY = (rectSizeY / 15) * 12;
+	
+}
---
+++ 1411029-1412122/src/rendering/IView.java
@@ -0,0 +1,7 @@
+package rendering;
+
+import java.awt.Graphics2D;
+
+public interface IView {
+	public void render(Graphics2D g2d);
+}
