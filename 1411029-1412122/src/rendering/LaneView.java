a0875fcc3ca59be85666c2f622d106db5daa892a
Marcelo Paulon <marcelopaulon@outlook.com>
2016-05-14 04:49:18 -0300

    Coordenadas - lane

--- 1411029-1412122/src/rendering/LaneView.java
+++ 1411029-1412122/src/rendering/LaneView.java
@@ -1,9 +1,11 @@
 package rendering;
 
 import java.awt.Graphics2D;
+import java.util.Hashtable;
 
 import boardInfo.Lane;
 import gfx.GameColor;
+import utils.Coordinate;
 
 public class LaneView extends View {
 	
@@ -18,53 +20,99 @@ public class LaneView extends View {
 		this.yellowLane = yellowLane;
 		this.blueLane = blueLane;
 		this.redLane = redLane;
+		
+		calculateGreenLaneCoordinates();
+		calculateYellowLaneCoordinates();
+		calculateBlueLaneCoordinates();
+		calculateRedLaneCoordinates();
 	}
+		
+	private Hashtable<Integer, Coordinate> greenLaneCoordinates = new Hashtable<Integer, Coordinate>();
+	private Hashtable<Integer, Coordinate> yellowLaneCoordinates = new Hashtable<Integer, Coordinate>();
+	private Hashtable<Integer, Coordinate> blueLaneCoordinates = new Hashtable<Integer, Coordinate>();
+	private Hashtable<Integer, Coordinate> redLaneCoordinates = new Hashtable<Integer, Coordinate>();
 	
-	private void renderGreenLane(Graphics2D g2d)
+	private void calculateGreenLaneCoordinates()
 	{
 		float x = yardSize + squareSize;
 		int position = 0;
 		
 		for(float y = squareSize; y < yardSize; y += squareSize)
 		{
-			new SquareView(greenLane.getSquareAt(position), x, y, GameColor.Green, false).render(g2d);
+			greenLaneCoordinates.put(position, new Coordinate(x, y));
 			position++;
 		}
 	}
 	
-	private void renderYellowLane(Graphics2D g2d)
+	private void calculateYellowLaneCoordinates()
 	{
 		float y = yardSize + squareSize;
 		int position = 0;
 		
 		for(float x = yardSize + 7 * squareSize; x >= yardSize + 3 * squareSize; x -= squareSize)
 		{
-			new SquareView(yellowLane.getSquareAt(position), x, y, GameColor.Yellow, false).render(g2d);
+			yellowLaneCoordinates.put(position, new Coordinate(x, y));
 			position++;
 		}
 	}
 	
-	private void renderBlueLane(Graphics2D g2d)
+	private void calculateBlueLaneCoordinates()
 	{
 		float x = yardSize + squareSize;
 		int position = 0;
 		
 		for(float y = yardSize + 7 * squareSize; y >= yardSize + 3 * squareSize; y -= squareSize)
 		{
-			new SquareView(blueLane.getSquareAt(position), x, y, GameColor.Blue, false).render(g2d);
+			blueLaneCoordinates.put(position, new Coordinate(x, y));
 			position++;
 		}
 	}
 	
-	private void renderRedLane(Graphics2D g2d)
+	private void calculateRedLaneCoordinates()
 	{
 		float y = yardSize + squareSize;
 		int position = 0;
 		
 		for(float x = squareSize; x < squareSize + 5 * squareSize; x += squareSize)
 		{
-			new SquareView(redLane.getSquareAt(position), x, y, GameColor.Red, false).render(g2d);
+			redLaneCoordinates.put(position, new Coordinate(x, y));
 			position++;
+		}
+	}
+	
+	private void renderGreenLane(Graphics2D g2d)
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			Coordinate coordinate = greenLaneCoordinates.get(i);
+			new SquareView(greenLane.getSquareAt(i), (int) coordinate.getX(), (int) coordinate.getY(), GameColor.Green, false).render(g2d);
+		}
+	}
+	
+	private void renderYellowLane(Graphics2D g2d)
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			Coordinate coordinate = yellowLaneCoordinates.get(i);
+			new SquareView(yellowLane.getSquareAt(i), (int) coordinate.getX(), (int) coordinate.getY(), GameColor.Yellow, false).render(g2d);
+		}
+	}
+	
+	private void renderBlueLane(Graphics2D g2d)
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			Coordinate coordinate = blueLaneCoordinates.get(i);
+			new SquareView(blueLane.getSquareAt(i), (int) coordinate.getX(), (int) coordinate.getY(), GameColor.Blue, false).render(g2d);
+		}
+	}
+	
+	private void renderRedLane(Graphics2D g2d)
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			Coordinate coordinate = redLaneCoordinates.get(i);
+			new SquareView(redLane.getSquareAt(i), (int) coordinate.getX(), (int) coordinate.getY(), GameColor.Red, false).render(g2d);
 		}
 	}
 	
