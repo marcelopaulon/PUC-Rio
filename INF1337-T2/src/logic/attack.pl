6a6b80bfc0475b1de2cf573482e1bfa17184cf1e
Renan da Fonte <renandafonte@hotmail.com>
2016-05-26 20:22:40 -0300

    Modificações no shoot(ShootingActionResult)
    Retirada a diminuição de energia que ocorria quando o agente atirava

--- INF1337-T2/src/logic/attack.pl
+++ INF1337-T2/src/logic/attack.pl
@@ -1,9 +1,9 @@
 shoot(ShootingActionResult) :- 
-			     curPosition(X, Y, _), adjacent(X, Y, XX, YY), hasEnemy(XX, YY), curEnergy(AgentEnergy),
+			     curPosition(X, Y, _), adjacent(X, Y, XX, YY), hasEnemy(XX, YY),
 			     ShootingActionResult = attack,
 			     random_between(20, 50, EnemyDamage),
 				 (
-				 	(enemyCell(EnemyDamage, EnemyEnergy, XX, YY), AgentEnergyAfter is AgentEnergy - EnemyDamage, retract(curEnergy(AgentEnergy)), assert(curEnergy(AgentEnergyAfter)),
+				 	(enemyCell(EnemyDamage, EnemyEnergy, XX, YY),
 				 		EnemyEnergyAfter is EnemyEnergy - EnemyDamage,
 				 		(
 				 			(EnemyEnergyAfter < 1, retract(enemyCell(_,_, XX, YY)), assert(floorCell(XX, YY)), ShootingActionResult = kill);
