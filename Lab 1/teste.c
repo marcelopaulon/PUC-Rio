cf90f341ef5a4a25d4803a527b48b9842f1cb9a1
= <marcelopaulon@snaydley.com>
2016-09-06 14:06:28 -0300

    Lab 1

--- Lab 1/teste.c
+++ Lab 1/teste.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 #include "fcos.h"
 
 #define SIZE 10
 
 #define MAX 0.07969262624616703
+
+#define DEBUG 0
+
 int main(void)
 {
 	double i;
 	int countTests = 0, countErrors = 0;
-	double error, difference;
+	double error, residue, difference, fcosResult, cosResult;
 	
-	printf("1) fcos(x):\n");
+	printf("1) ");
 
-	for(i = 0.0; i < 3.14; countTests++, i += 0.01)
+	if(DEBUG == 1) printf("fcos(x):\n");
+
+	for(i = 0.0; i < 3.14; countTests++, i += 0.001)
 	{
+		fcosResult = fcos(i);
+		cosResult = cos(i);
+
 		difference = i - PI_2;
-		error = -(difference * difference * difference * difference * difference) / 120;
-		if(error > MAX) {
-			printf("FAIL: ");
+		error = fabs(fcosResult - cosResult);
+		residue = -(difference * difference * difference * difference * difference) / 120;
+
+		if(error > MAX)
+		{
+			printf("Value exceeded maximum error. Exiting.\n");
+			exit(-1);
+		}
+
+		if(error > fabs(residue)) {
+			if(DEBUG == 1) printf("FAIL: ");
 			countErrors++;
 		}
 		else {
-			printf("PASS: ");
+			if(DEBUG == 1) printf("PASS: ");
 		}
-		printf("fcos(%.3f) = %.16g (error = %.16g)\n", i, fcos(i), error);
+
+		if(DEBUG == 1) printf("fcos(%.4g) = %.16g (error = %.16g; residue = %.16g)\n", i, fcosResult, error, residue);
 	}
+
+	if(DEBUG == 1) printf("\n");
 	
-	printf("\n%d/%d tests passed", countTests-countErrors, countTests);
+	printf("%d/%d tests passed", countTests-countErrors, countTests);
 	printf("\n2) %.16g\n", 9.4 - 9 - 0.4);
 
 	return 0;
