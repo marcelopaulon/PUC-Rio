3efcd7bfa4e46238529e43b1d981ba4b05b76222
Marcelo Paulon <marcelopaulon@outlook.com>
2016-10-07 00:54:02 -0300

    Correção

--- Lab 5/Lab 5/interp.h
+++ Lab 5/Lab 5/interp.h
@@ -6,4 +6,8 @@ double ** fatoracao (int n, double **A);
 
 double * substituicao (int n, double **A, double **P, double *b);
 
-double * interpola (int n, double *px, double *py);double avalia (int n, double *c, double x);double lagrange (int n, double *px, double *py, double x);
+double * interpola (int n, double *px, double *py);
+
+double avalia (int n, double *c, double x);
+
+double lagrange (int n, double *px, double *py, double x);
