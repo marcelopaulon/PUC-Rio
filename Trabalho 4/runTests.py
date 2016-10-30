1d90d18865b761eafcfdbcbb2ae7c919ba65fc74
Marcelo Paulon <marcelopaulon@outlook.com>
2016-10-30 00:05:59 -0200

    Testes

--- Trabalho 4/runTests.py
+++ Trabalho 4/runTests.py
@@ -38,11 +38,20 @@ for file in sorted(os.listdir(testDir), key=lambda x: int(x.split('.', 1)[0])):
         totalTests = totalTests + 1
         print('\nTest ' + file + ': ')
         os.system('./teste < ' + testDir + '/' + file + ' > output.txt')
-        with open('output.txt') as f:
-            s = f.readlines()
-            if s[0] == 'PASS':
-                failedTests = failedTests + 1
-            print(s)
 
+        with open('output.txt') as f:
+             output = f.readlines()
+        with open(gabsDir + '/' + os.path.splitext(file)[0] + '.notmongalog.txt') as f:
+             expected = f.readlines()
+
+        differences = 0
+        for line in difflib.unified_diff(output, expected, fromfile='output', tofile='expected', lineterm=''):
+            print(line)
+            differences = differences + 1
+        if differences == 0:
+            print("\nSUCCESS!\n")
+        else:
+            print("\nFAIL\n")
+            failedTests = failedTests + 1
 
 print("\nFinished! " + str(totalTests - failedTests) + '/' + str(totalTests) + ' tests passed.\n')
