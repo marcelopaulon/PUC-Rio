8abd5d270d2c62068dd76e5cd33b00068ff6426f
Marcelo Paulon <marcelopaulon@outlook.com>
2016-12-08 17:13:43 -0200

    Trabalho 2

--- Trabalho 2/1411029-1412122/src/game/GameControl.java
+++ Trabalho 2/1411029-1412122/src/game/GameControl.java
@@ -1,4 +1,4 @@
-﻿package game;
+package game;
 
 import boardInfo.Board;
 import boardInfo.Dice;
--- Trabalho 2/1411029-1412122/src/notifications/Notifications.java
+++ Trabalho 2/1411029-1412122/src/notifications/Notifications.java
@@ -1,4 +1,4 @@
-﻿package notifications;
+package notifications;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
