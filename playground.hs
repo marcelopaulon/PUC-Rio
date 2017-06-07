e1db60bfd9025c0229c0b0c58b1144da7de89f5b
Marcelo Paulon <marcelopaulon@outlook.com>
2017-06-06 21:14:05 -0300

    Update early functional language cps.txt

--- playground.hs
+++ playground.hs
@@ -1,4 +1,4 @@
-data Nat = Zero 
+data Nat = Zero
          | Inc Nat
 soma::Nat->Nat->Nat
 soma Zero y = y
@@ -8,15 +8,17 @@ dec::Nat->Nat
 dec Zero = Zero
 dec (Inc(x)) = x
 
+printNat::Nat->String
 printNat Zero = "Z"
 printNat (Inc x) = "Inc " ++ printNat x
 
 sort1 [] = []
 sort1 (x:xs) = sort1 (filter ((>=) x) xs) ++ (x:(sort1 (filter ((<) x) xs)))
 
-printLista [] = "[]"
-printLista (x:xs) = show x ++ ":" ++ printLista xs
+printList::[Int]->[Char]
+printList [] = "[]"
+printList (x:xs) = show x ++ ":" ++ printList xs
 
-main = print $ printLista $ sort1 [3,2,9,1,2,2,3,8]
-
---main = print $ printNat $ dec (Inc(Inc(Zero)))
+main::IO ()
+main = (print $ printList $ sort1 [3,2,9,1,2,2,3,8]) >> -- this is equivalent to `... >>= \_ -> ...`
+       (print $ printNat $ dec (Inc(Inc(Zero))))
