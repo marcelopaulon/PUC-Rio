ffcedbc58e0ba0df1d4de363400030a45cef64a3
Renan da Fonte <renandafonte@hotmail.com>
2017-12-05 19:45:57 -0200

    Primeira cena com player andando

---
+++ CEZIS-Game/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float thresholdMove = 0.5f; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //a, w, s, d
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move *= Time.deltaTime;
+
+        transform.Translate(move);
+
+        // tiro
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Tiro");
+        }
+
+        // defesa
+	}
+}
