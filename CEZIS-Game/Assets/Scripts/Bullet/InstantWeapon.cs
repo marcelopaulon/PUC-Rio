c6d177b992e80c87a49721ce41b716f40674b6d9
psyny <psyny@hotmail.com>
2017-12-17 16:45:51 -0200

    Leve randomizada na direcao do tiro do inimigo

--- CEZIS-Game/Assets/Scripts/Bullet/InstantWeapon.cs
+++ CEZIS-Game/Assets/Scripts/Bullet/InstantWeapon.cs
@@ -4,10 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InstantWeapon : Weapon {
+    private float centerDirection = Mathf.PI * 1.5f;
+    private float weaponImprecision = Mathf.PI / 40;
 
     public override void _fire()
     {
-        GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        float rand = UnityEngine.Random.Range(-1.0f, 1.0f);
+        float piDir = centerDirection + (weaponImprecision * rand);
+        Vector3 dir = Vector3.zero;
+        dir.z = Mathf.Sin(piDir);
+        dir.x = Mathf.Cos(piDir);
+        bullet.GetComponent<Bullet>().direction = dir;
     }
 
     private void OnDrawGizmos()
