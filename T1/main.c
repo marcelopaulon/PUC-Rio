6c036c14ec66a15dcc632faf80b5d52b1cd04a0b
Marcelo Paulon <marcelopaulon@users.noreply.github.com>
2018-09-12 20:42:55 +0000

    wip

--- T1/main.c
+++ T1/main.c
@@ -1,11 +1,20 @@
 #include <limitedbuffer.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+#define NO_SLEEP 0
+#define SLEEP_ALL 0x1
+#define SLEEP_PRODUCERS 0x10
+#define SLEEP_CONSUMERS 0x100
 
 typedef struct consumerInfo_t {
     LBUF *lbuf;
     int threadId;
+    int mode;
+    int nDeposits;
 } consumerInfo;
 
 /* this function is run by the second thread */
@@ -16,10 +25,12 @@ void *consume(void *args)
   /* increment x to 100 */
   consumerInfo *cInfo = (consumerInfo *)args;
 
-  for(int i = 0; i < 8; i++) {
+  for(int i = 0; i < cInfo->nDeposits; i++) {
       int a = lbuf_consume(cInfo->lbuf, cInfo->threadId);
-      //sleep(1);
       printf("[%d] Consumed %d \n", cInfo->threadId, a);
+      if(cInfo->mode & (SLEEP_ALL | SLEEP_CONSUMERS)) {
+        sleep(1);
+      }
   }
 
   return NULL;
@@ -29,9 +40,61 @@ void ldebug(LBUF *lbuf) {
     printf("\nn=%d p=%d c=%d\nwaitForDeposit=%d waitForRead=%d\n", lbuf->n, lbuf->p, lbuf->c, lbuf->waitForDeposit, lbuf->waitForRead);
 }
 
-int main() {
-  // call a function in another file
-  LBUF *lbuf = lbuf_create(3, 2, 3);
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("USAGE: lbuf nProducers nConsumers [ mode] \n"); 
+        printf("  mode: SLEEP_ALL - each consumer and producer will sleep for 1 second between requests \n");
+        printf("        SLEEP_PRODUCERS - each producer will sleep for 1 second between requests \n");
+        printf("        SLEEP_CONSUMERS - each consumer will sleep for 1 second between requests \n");
+        printf("        NO_SLEEP - (default) each consumer and producer will execute without any sleep calls\n");
+        printf("\n Description: simulates de limited buffer (limitedbuffer.h) for a specified number of producers/consumers\n\n");
+        exit(0);
+    }
+
+    int nProducers = -1;
+    int nConsumers = -1;
+
+    int ret = 0;
+    ret = sscanf(argv[1], "%d", &nProducers);
+    if (ret != 1) return -1;
+    ret = sscanf(argv[2], "%d", &nConsumers);
+    if (ret != 1) return -1;     
+    
+    if(nProducers < 1 || nConsumers < 1) {
+        printf("Error: nProducers and nConsumers should be greater than 0");
+        exit(-1);
+    }
+
+    int mode = NO_SLEEP;
+
+    if(argc == 4) {
+        char *modeStr = (char *) malloc(1024 * sizeof(char));
+        ret = sscanf(argv[3], "%s", modeStr);
+        if (ret != 1) return -1;
+
+        if(strcmp(modeStr, "NO_SLEEP") == 0)
+            mode = NO_SLEEP;
+        else if(strcmp(modeStr, "SLEEP_ALL") == 0)
+            mode = SLEEP_ALL;
+        else if(strcmp(modeStr, "SLEEP_PRODUCERS") == 0)
+            mode = SLEEP_PRODUCERS;
+        else if(strcmp(modeStr, "SLEEP_CONSUMERS") == 0)
+            mode = SLEEP_CONSUMERS;
+        else {
+            free(modeStr);
+            printf("Invalid mode\n");
+            exit(-1);
+        }
+
+        free(modeStr);
+    }
+    else if(argc != 3) {
+        printf("Unknown combination of parameters\n");
+        exit(-1);
+    }
+
+    // call a function in another file
+    LBUF *lbuf = lbuf_create(3, nProducers, nConsumers);
 
 
     printf("Deposit start\n");
@@ -42,40 +105,21 @@ int main() {
     lbuf_deposit(lbuf, 3);
     printf("Deposit 3\n");
 
-  consumerInfo cInfo1;
-  cInfo1.lbuf = lbuf;
-  cInfo1.threadId = 0;
+    for(int i = 0; i < nConsumers; i++) {
+        consumerInfo cInfo;
+        cInfo.lbuf = lbuf;
+        cInfo.threadId = i;
+        cInfo.mode = mode;
+        cInfo.nDeposits = 8;
 
-  /* this variable is our reference to the second thread */
-  pthread_t n1_thread;
+        pthread_t thread;
 
-  if(pthread_create(&n1_thread, NULL, consume, &cInfo1)) {
-    fprintf(stderr, "Error creating thread\n");
-    return 1;
-  }
+        if(pthread_create(&thread, NULL, consume, &cInfo)) {
+            fprintf(stderr, "Error creating thread\n");
+            return 1;
+        }
 
-    consumerInfo cInfo2;
-    cInfo2.lbuf = lbuf;
-    cInfo2.threadId = 1;
-
-    /* this variable is our reference to the third thread */
-    pthread_t n2_thread;
-sleep(2);
-    if(pthread_create(&n2_thread, NULL, consume, &cInfo2)) {
-        fprintf(stderr, "Error creating thread\n");
-        return 1;
-    }
-
-    consumerInfo cInfo3;
-    cInfo3.lbuf = lbuf;
-    cInfo3.threadId = 2;
-
-    /* this variable is our reference to the third thread */
-    pthread_t n3_thread;
-sleep(3);
-    if(pthread_create(&n3_thread, NULL, consume, &cInfo3)) {
-        fprintf(stderr, "Error creating thread\n");
-        return 1;
+        sleep(1 + i);
     }
 
     lbuf_deposit(lbuf, 4);
@@ -90,11 +134,12 @@ sleep(3);
     lbuf_deposit(lbuf, 8);
     printf("Deposit 8\n");
 
-ldebug(lbuf);
-while(1) {
-    printf("Waiting...");
     ldebug(lbuf);
-    sleep(1);
-}
-  return(0);
+    while(1) {
+        printf("Waiting...");
+        ldebug(lbuf);
+        sleep(1);
+    }
+    
+    return(0);
 }
