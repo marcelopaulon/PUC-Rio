9eab675bab60d8022928171c72ab438021514042
Marcelo Paulon <marcelo.paulon@gmail.com>
2018-10-14 21:00:35 -0300

    adding benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+mpirun -np 1 ./aquad1 0 1
+mpirun -np 1 ./aquad1 0 1
+mpirun -np 2 ./aquad1 0 1
+mpirun -np 4 ./aquad1 0 1
+mpirun -np 8 ./aquad1 0 1
+mpirun -np 2 ./aquad2 0 1 2
+mpirun -np 4 ./aquad2 0 1 4
+mpirun -np 8 ./aquad2 0 1 8
+mpirun -np 2 ./aquad2 0 1 1000
+mpirun -np 4 ./aquad2 0 1 1000
+mpirun -np 8 ./aquad2 0 1 1000
