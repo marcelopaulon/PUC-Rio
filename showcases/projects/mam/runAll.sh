a92acd2ce9f11500e134d2eac2a2450a19fdab8e
Marcelo Paulon <marcelopaulon@gmail.com>
2020-12-18 20:59:25 -0300

    Multiple map variations

--- showcases/projects/mam/runAll.sh
+++ showcases/projects/mam/runAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #inet omnetpp.ini -u Cmdenv -c "MAM50-BMRELAY-13mps"
 
-cat mamSim.ini | ggrep -P '\[Config ' | sed 's/\[Config \(.*\)\]/ omnetpp.ini -u Cmdenv -c "\1"/' | xargs -L1 inet --release
+cat mamSim.ini | ggrep -P '\[Config ' | sed 's/\[Config \(.*\)\]/ mamSim.ini -u Cmdenv -c "\1"/' | xargs -L1 inet --release
 
