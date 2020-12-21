8e6f524d690e583ae45fdd008645ed7409f8b275
Marcelo Paulon <marcelopaulon@gmail.com>
2020-12-21 18:32:29 -0300

    Multiple map variations

--- showcases/projects/mam/genCommands.sh
+++ showcases/projects/mam/genCommands.sh
@@ -1,4 +1,4 @@
-rm commands.txt
+rm -f commands.txt
 touch commands.txt
 cat mamSim.ini | ggrep -P '\[Config ' | sed 's/\[Config \(.*\)\]/ inet --release mamSim.ini -u Cmdenv -c "\1"/' > commands.txt.tmp
 cat commands.txt.tmp | while read line; do echo "${line} && rm -f results/*.vec && rm -f results/*.vci">>commands.txt; done
